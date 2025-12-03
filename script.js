ae408f4b364de86479747c6458e89fe69b22d476
tai928 <taisei.study928@gmail.com>
2025-12-03 22:14:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,11 @@
 // StepLink 共通スクリプト  (script.js)
 // ========================================
 
-// supabaseClient.js に任せるので何も書かない
-console.log("profile.js loaded");
+// supabaseClient.js
+const SUPABASE_URL = "https://ngtthuwmqdcxgddlbsyo.supabase.co";
+const SUPABASE_KEY = "sb_publishable_YJzguO8nmmVKURa58cKwVw__9ulKxI6";
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // ログイン中ユーザー
 let currentUser = null;
