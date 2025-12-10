1ff0c04e4ccae995dbd618b70e0754a66b5551b8
tai928 <taisei.study928@gmail.com>
2025-12-10 21:15:52 +0900

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,5 +0,0 @@
-// supabaseClient.js
-const SUPABASE_URL = "https://ngtthuwmqdcxgddlbsyo.supabase.co";
-const SUPABASE_KEY = "sb_publishable_YJzguO8nmmVKURa58cKwVw__9ulKxI6";
-
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
