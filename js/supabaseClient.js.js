6b771d7db77455ae271f0c1e76701e3a350ea326
tai928 <taisei.study928@gmail.com>
2026-03-17 00:05:45 +0900

    Delete js directory

--- js/supabaseClient.js.js
+++
@@ -1,4 +0,0 @@
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
-
-window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
